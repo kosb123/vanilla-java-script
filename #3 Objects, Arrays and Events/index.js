1299d27871dc92b5e829c27922bdd31aa44d3876
kosb123 <yjy980425@gmail.com>
2020-11-25 14:35:44 +0900

    #3 Objects, Arrays and Events

--- #3 Objects, Arrays and Events/index.js
+++ #3 Objects, Arrays and Events/index.js
@@ -1,22 +1,22 @@
 const title = document.querySelector("h2");
 
-const colors = ['red', 'blue', 'green', 'tomato'];
+const colorsArray = ['red', 'blue', 'green', 'tomato'];
 
 const superEventHandler = {
     mouseOnTop : function(){
-        title.style.color = colors[0];
+        title.style.color = colorsArray[0];
         title.innerHTML = 'The mouse is here!';
     },
     mouseLeaves : function(){
-        title.style.color = colors[1];
+        title.style.color = colorsArray[1];
         title.innerHTML = 'The mouse is gone!';
     },
     windowResize : function(){
-        title.style.color = colors[2];
+        title.style.color = colorsArray[2];
         title.innerHTML = 'You Just Resized!';
     },
     mouseRightClick : function(){
-        title.style.color = colors[3]; 
+        title.style.color = colorsArray[3]; 
         title.innerHTML = 'That was a right click!';
     }
 }
