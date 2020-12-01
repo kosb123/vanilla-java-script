e146069b02ca2ab63b28429e84296a6150cd3cf8
kosb123 <yjy980425@gmail.com>
2020-12-01 23:07:09 +0900

    I don't know everything...

--- #6. Events, localStorage/index.js
+++ #6. Events, localStorage/index.js
@@ -1,9 +1,11 @@
-const select = document.querySelector("select");
+const select = document.querySelector(".select");
 const selectValue = select.options[select.selectedIndex].value;
 
 function saveName() {
     localStorage.setItem('country', selectValue);
     console.log(selectValue)
 }
+
+saveName();
 
 select.addEventListener("change", saveName);
